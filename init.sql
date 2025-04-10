fc11a0948aa41297c2cef0353668dcb22b33673e
Jorge <erudito.tv@gmail.com>
2025-04-10 14:31:30 -0500

    v1

--- init.sql
+++ init.sql
@@ -17,7 +17,7 @@ VALUES
   ('FANTASIA'),
   ('DOCUMENTAL');
 
--- PELICULAS (con cover y url como BLOBs)
+-- PELÍCULAS (con cover y url como BLOBs)
 CREATE TABLE
   IF NOT EXISTS movies (
     movie_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -35,8 +35,8 @@ VALUES
   (
     'MAN OF HONOR',
     2000,
-    'https://pics.filmaffinity.com/men_of_honor-584186623-large.jpg',
-    './videos/Cap_1.mp4',
+    'https://m.media-amazon.com/images/I/91M9JytQi2L._AC_UF894,1000_QL80_.jpg',
+    'https://firebasestorage.googleapis.com/v0/b/imagenes-cd065.appspot.com/o/Medias%2FVideos%2FPete%20Bellis%20%26%20Tommy%20-%20You%20Should%20Know%20(Costa%20Mee%20Remix).mp4?alt=media&token=c1348dc4-ba43-4af5-9f65-1880584b7dd1',
     1
   );
 
@@ -51,6 +51,22 @@ CREATE TABLE
     FOREIGN KEY (gender_id) REFERENCES genders (gender_id)
   );
 
+INSERT INTO
+  content_types (title, descripcion, cover, gender_id)
+VALUES
+  (
+    'Anime',
+    'JUJUTSU KAISEN',
+    'https://latanime.org/thumbs/imagen/jujutsu-kaisen-s2-latino-1690490946.jpg',
+    7
+  ),
+  (
+    'Anime',
+    'JUJUTSU KAISEN',
+    'https://latanime.org/thumbs/imagen/jujutsu-kaisen-s2-castellano-1692305959.jpg',
+    7
+  );
+
 -- TEMPORADAS DE SERIES O ANIMES
 CREATE TABLE
   IF NOT EXISTS seasons (
@@ -61,6 +77,12 @@ CREATE TABLE
     FOREIGN KEY (content_type_id) REFERENCES content_types (content_type_id)
   );
 
+INSERT INTO
+  seasons (content_type_id, season_number, title)
+VALUES
+  (1, 1, 'Temporada 1'),
+  (2, 2, 'Temporada 2');
+
 -- EPISODIOS DE TEMPORADAS
 CREATE TABLE
   IF NOT EXISTS episodes (
@@ -70,4 +92,26 @@ CREATE TABLE
     title TEXT NOT NULL,
     url TEXT NOT NULL,
     FOREIGN KEY (season_id) REFERENCES seasons (season_id)
+  );
+
+INSERT INTO
+  episodes (season_id, episode_number, title, url)
+VALUES
+  (
+    1,
+    1,
+    'Capitulo 1',
+    'https://firebasestorage.googleapis.com/v0/b/imagenes-cd065.appspot.com/o/Peliculas%2FVideos%2FCap_1.mp4?alt=media&token=6e063fc6-29c2-4522-be07-2e45cdbd81e8'
+  ),
+  (
+    2,
+    1,
+    'Capitulo 1',
+    'https://firebasestorage.googleapis.com/v0/b/imagenes-cd065.appspot.com/o/Peliculas%2FVideos%2Fvideo.mp4?alt=media&token=5f0bae50-f142-4052-ab75-7456b4d513eb'
+  ),
+  (
+    2,
+    2,
+    'Capitulo 2',
+    'https://firebasestorage.googleapis.com/v0/b/imagenes-cd065.appspot.com/o/Peliculas%2FVideos%2FCap_2.mp4?alt=media&token=7c1b4c8e-5b78-48e8-ab4f-e4b43571998a'
   );
