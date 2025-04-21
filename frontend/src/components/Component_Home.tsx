8cb98588ebed824c77649c669ee8eba734608da4
Jorge <erudito.tv@gmail.com>
2025-04-21 08:29:09 -0500

    v1

--- frontend/src/components/Component_Home.tsx
+++ frontend/src/components/Component_Home.tsx
@@ -1,24 +1,9 @@
 import "../styles/App.css";
-<<<<<<< HEAD
-import { Component_Content } from "./Component_Content";
-import { Component_Footer } from "./Component_Footer";
-import { Component_Header } from "./Component_Header";
-import { Component_Menu } from "./Component_Menu";
-=======
->>>>>>> 9079f832cd9966cc68761a0884cd2e9e88e6da91
 
 export const Component_Home = () => {
   return (
     <div className="app-container">
-      <div className="main-content">
-<<<<<<< HEAD
-        <Modal_Content />
-        <Modal_Movie />
-        <Modal_Auth />
-        <Component_Content/>
-=======
->>>>>>> 9079f832cd9966cc68761a0884cd2e9e88e6da91
-      </div>
+      <div className="main-content"></div>
     </div>
   );
 };
