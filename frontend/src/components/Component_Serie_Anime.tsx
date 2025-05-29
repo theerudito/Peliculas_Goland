a60f52ad187fe53c6a8736bd3ead4b6337b1c6c5
Jorge <erudito.tv@gmail.com>
2025-05-29 13:48:12 -0500

    v1

--- frontend/src/components/Component_Serie_Anime.tsx
+++ frontend/src/components/Component_Serie_Anime.tsx
@@ -4,9 +4,11 @@ import { useContent } from "../store/useContent";
 import cover from "../assets/logo.webp";
 import { useAuth } from "../store/useAuth";
 import { Component_Content } from "./Component_Content";
+import { Component_Search } from "./Component_Search";
 
 export const Component_Serie_Anime = () => {
-  const { list_content_type, getContent_Type, openContent, openContent_Type } = useContent((state) => state);
+  const { list_content_type, getContent_Type, openContent, openContent_Type } =
+    useContent((state) => state);
   const { isLogin } = useAuth((state) => state);
 
   useEffect(() => {
@@ -15,22 +17,13 @@ export const Component_Serie_Anime = () => {
 
   return (
     <div className="app-container">
-
       {openContent === false ? (
-
         <div>
-          <div className="container-header-search-box">
-            <input type="text" placeholder="Buscar..." />
-            <button className="container-header-search-btn">
-              <i className="bi bi-search"></i>
-            </button>
-          </div>
+          <Component_Search />
 
           <div className="main-content">
             <div className="container-content">
               <div className="container-body">
-
-
                 {list_content_type.map((item) => (
                   <div key={item.content_id} className="container-card">
                     <img
@@ -46,32 +39,29 @@ export const Component_Serie_Anime = () => {
                       <div className="card-play">
                         <i
                           className="bi bi-play-circle"
-                          onClick={() => openContent_Type(true, item.content_id)}
+                          onClick={() =>
+                            openContent_Type(true, item.content_id)
+                          }
                         ></i>
                       </div>
                       <div className="card-container-button">
-                        {
-                          isLogin && (
-                            <>
-                              <i className="bi bi-pencil-square"></i>
-                              <i className="bi bi-trash"></i>
-                            </>
-                          )
-                        }
-
+                        {isLogin && (
+                          <>
+                            <i className="bi bi-pencil-square"></i>
+                            <i className="bi bi-trash"></i>
+                          </>
+                        )}
                       </div>
                     </div>
                   </div>
                 ))}
-
-
               </div>
             </div>
           </div>
-
         </div>
-      ) : (<Component_Content />)
-      }
-    </div >
+      ) : (
+        <Component_Content />
+      )}
+    </div>
   );
 };
